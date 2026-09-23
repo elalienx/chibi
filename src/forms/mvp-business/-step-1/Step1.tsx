74627935e8705f3f41ce3c2366e06cdfd42681d9
Eduardo Alvarez <el_alienx@hotmail.com>
2026-09-23 20:20:06 +0200

    creating boilerplate

---
+++ src/forms/mvp-business/-step-1/Step1.tsx
@@ -0,0 +1,19 @@
+/**
+ * TODO
+ * Components:
+ * - Input.number: Välj lånesumma:, min 50_000, default 600_000, 30_000_000
+ * - Slider: min 50_000, default loan_amount current value,max 30_000
+ * - Input.number: Välj lånetid:, 1.5, år.
+ * - Button.call-to-action: Påbörja ansökan, ->
+ * - Small: (Ansökan är inte bindande)
+ *
+ * Notes:
+ * - This step lacks a header because it is the first step, so skip it.
+ * - The slider component does not exist, ignore it.
+ * - The call to action and small text are part of the footer.
+ * - There is not <hr/> divider in this page.
+ */
+
+export default function Step1() {
+  return <div>step 1</div>;
+}
