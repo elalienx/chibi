b262f75cb8e8bbaf0596a064efd83e281b7e9d79
Eduardo Alvarez <eduardo.alvarez.nowak@lendo.se>
2026-09-26 14:12:08 +0200

    removed unused files

--- src/forms/mvp-business/types/Purpose.ts
+++
@@ -1,5 +0,0 @@
-// Project files
-import type purposes from "../data/purposes";
-
-/** The reason an applicant choose to apply for loan. */
-export type Purpose = (typeof purposes)[number]["value"];
