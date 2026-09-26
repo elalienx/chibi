a5fc61b022ff38985940f554e769ec2a9a073a9f
Eduardo Alvarez <eduardo.alvarez.nowak@lendo.se>
2026-09-26 13:49:37 +0200

    adding test

--- src/forms/mvp-business/-step-3/schema.ts
+++ src/forms/mvp-business/-step-3/schema.ts
@@ -2,11 +2,7 @@
 import * as v from "valibot";
 
 // Fields
-const company_org_number = v.pipe(
-  v.string("Vänligen välj bolag."),
-  v.nonEmpty("Vänligen välj bolag."),
-  v.check((input) => ["5590245535", "5590480512", "5561081620"].includes(input), "Vänligen välj ett giltigt bolag."),
-);
+const company_org_number = v.pipe(v.string("Vänligen välj bolag."), v.nonEmpty("Vänligen välj bolag."));
 
 // Schema
 const schema = v.object({ company_org_number });
