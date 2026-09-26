f5e812695fce8357d69a2f50e4b2c307a7bdb946
Eduardo Alvarez <eduardo.alvarez.nowak@lendo.se>
2026-09-26 14:37:47 +0200

    remove details in comment

--- src/forms/mvp-business/types/Step.ts
+++ src/forms/mvp-business/types/Step.ts
@@ -1,2 +1,2 @@
-/** A list of available steps in this form flow. Steps 1–5 are implemented in this demo. */
+/** A list of available steps in this form flow. */
 export type Step = "intro-step" | "step-1" | "step-2" | "step-3" | "step-4" | "step-5" | "success-step";
