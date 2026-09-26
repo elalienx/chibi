5ebe91f46764aca2a030bc9de5fa2bfb16aa2544
Eduardo Alvarez <eduardo.alvarez.nowak@lendo.se>
2026-09-26 14:40:14 +0200

    updating purpose list

--- src/forms/mvp-business/data/purposes.ts
+++ src/forms/mvp-business/data/purposes.ts
@@ -1,19 +1,14 @@
 /** A list of reasons to apply for a loan. */
 const purposes = [
-  { value: "cashFlow", label: "Generell likviditet/kassaflöde" },
-  { value: "investmentSeason", label: "Investering säsong" },
-  { value: "purchaseInventory", label: "Inköp av lager" },
-  { value: "purchaseEquipment", label: "Inköp av maskiner/utrustning" },
+  { value: "purchase_equipment", label: "Maskiner & Utrustning" },
+  { value: "cash_flow", label: "Likviditet" },
+  { value: "finance_debt", label: "Finansiera skuld" },
+  { value: "investment_season", label: "Säsongsinvestering" },
+  { value: "purchase_inventory", label: "Inköp av lager" },
+  { value: "expansion", label: "Förändring & Tillväxt" },
   { value: "refurbishment", label: "Renovering av lokal" },
-  { value: "propertyAcquisition", label: "Fastighetsförvärv" },
-  { value: "marketing", label: "Hemsida/marknadsföring" },
-  { value: "permitApplication", label: "Tillståndsansökan" },
-  { value: "unexpectedExpenses", label: "Oväntade utgifter" },
-  { value: "expansion", label: "Omformation/expansion" },
-  { value: "financeDebt", label: "Finansiera skuld" },
-  { value: "hiring", label: "Anställa personal" },
+  { value: "property_acquisition", label: "Fastighetsförvärv" },
   { value: "acquisition", label: "Förvärv" },
-  { value: "other", label: "Övrigt" },
 ] as const;
 
 export default purposes;
